13cbb081593849797d4016f09ba9fea0618480dd
Abdelkhalek Aberhouch <a.aberhouch@gmail.com>
2022-07-04 14:29:57 -0400

    data at 101% &

--- src/load_data.py
+++ src/load_data.py
@@ -11,10 +11,10 @@ def load_and_save(config_path):
     raw_data_path = config["load_data"]["raw_dataset_csv"]
     print(f"100% des observations: {news.shape[0]}")
 
-    sample = config["load_data"]["sample_frac"]
+    samples = config["load_data"]["sample_frac"]
     random_state = config["base"]["random_state"]
 
-    news = news.sample(frac = sample , random_state = random_state)
+    news = news.sample(frac = samples, random_state = random_state)
 
     #news.reset_index(drop=True, inplace=True)
     print(news.head())
